3eb0819cdf788b085d33d423d4a77f8ba4f89c1b
Aleksey Seleznev <ferfa2@rambler.ru>
2022-07-06 17:54:41 +0300

    rename main_logic.py to engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,25 +1,25 @@
 import prompt
 
 
+REQ_NUMBER_CORRECT_ANSWERS = 3
+
+
 def game(game_rule, game):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print(game_rule)
-    max_stages = 3
     correct_answers = 0
-    req_number_correct_answers = 3
-    for i in range(max_stages):
+    for i in range(REQ_NUMBER_CORRECT_ANSWERS):
         number, answer = game()
         print(f'Question: {number}')
         response = prompt.string('Your answer: ')
         if response == answer:
             print('Correct!')
             correct_answers += 1
-            game()
         else:
             print(f"""'{response}' is wrong answer ;(. Correct answer was '{answer}'.
 Let's try again, {name}!""")
             break
-        if correct_answers == req_number_correct_answers:
-            print(f'Congratulations, {name}!')
+    else:
+        print(f'Congratulations, {name}!')
